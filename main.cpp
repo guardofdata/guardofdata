e0e6944ceb8750a2786922f33e6bce8afbb0120a
Alexander Tretyak <alextretyak@users.noreply.github.com>
2020-02-11 21:43:12 +1000

    Add --exit-on-close-button option (as Release build can also be used for debugging if speed is important)

--- main.cpp
+++ main.cpp
@@ -42,12 +42,10 @@ LRESULT CALLBACK main_wnd_proc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lp
         return 0;
 
     case WM_CLOSE:
-#ifdef _DEBUG
-        break;
-#else
+        if (wcsstr(GetCommandLine(), L" --exit-on-close-button"))
+            break;
         ShowWindow(hwnd, SW_HIDE);
         return 0; // to not process message by the system, which call DestroyWindow in DefWindowProc
-#endif
 
     case WM_TRAY:
         switch (lparam)
